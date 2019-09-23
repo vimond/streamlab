0fb4dffeb8d60e955b553fab6635cb6c526a50aa
Tor Erik Alræk <torerik@vimond.com>
2019-09-23 22:14:22 +0200

    Layout with resizable pane.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { RefObject } from 'react';
+import { ThemeProvider, ColorModeProvider } from '@chakra-ui/core';
+import {
+  Accordion,
+  AccordionHeader,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Alert,
+  AlertIcon,
+  Box, Button,
+  CSSReset, Flex, FormControl,
+  Heading, Input, Stack,
+  Text
+} from '@chakra-ui/core/dist';
+import PanelGroup from 'react-panelgroup';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Drawer for save/load.
+
+const SectionHeader: React.FC<{ header: string }> = ({ header }) => (
+  <AccordionHeader>
+    <Heading as="h2" fontWeight="normal" size="sm" margin={0} padding={1} flex="1" textAlign="left">
+      {header}
+    </Heading>
+    <AccordionIcon />
+  </AccordionHeader>
+);
+
+const App: React.FC = () => (
+  <ThemeProvider>
+    <ColorModeProvider value="light">
+      <CSSReset />
+      <PanelGroup borderColor="#eee" spacing={3} panelWidths={[{ resize: 'stretch', size: 100 }, { resize: 'stretch', size: 70 }]}>
+        <Accordion width="100%" defaultIndex={[1]} allowMultiple>
+          <AccordionItem>
+            <SectionHeader header="API lookup"/>
+            <AccordionPanel><Text>TODO</Text></AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <SectionHeader header="Stream details"/>
+            <AccordionPanel>
+              <form>
+                <Stack>
+                  <FormControl isRequired>
+                    <Input placeholder="Stream URL" type="url"/>
+                  </FormControl>
+                  <FormControl isRequired>
+                    <Input placeholder="DRM license URL" type="url" />
+                  </FormControl>
+                  <FormControl isRequired>
+                    <Input placeholder="Subtitles URL" type="url" />
+                  </FormControl>
+                  <Flex justify="center">
+                    <Button variantColor="green" mx={1}>Play</Button>
+                    <Button mx={1}>Clear</Button>
+                  </Flex>
+                </Stack>
+              </form>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <SectionHeader header="Stream proxy"/>
+            <AccordionPanel><Text>TODO</Text></AccordionPanel>
+          </AccordionItem>
+          <AccordionItem>
+            <SectionHeader header="Player options"/>
+            <AccordionPanel><Text>TODO</Text></AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+        <Box width="100%" height="100%" backgroundColor="gray.100">
+          <Heading textAlign="center" p={6} backgroundColor="gray.100">
+            Streamlab
+          </Heading>
+          <Alert status="info" alignItems="flex-start">
+            <AlertIcon />
+            Welcome to Streamlab.
+          </Alert>
+          <Alert status="info" alignItems="flex-start">
+            <AlertIcon />
+            Fill in an URL to the stream you want to test, and press Play.
+          </Alert>
+        </Box>
+      </PanelGroup>
+    </ColorModeProvider>
+  </ThemeProvider>
+);
 
 export default App;
---
+++ todo.md
@@ -0,0 +1,5 @@
+
+### Remember
+
+* Accordion folds with "active" settings should be marked.
+
